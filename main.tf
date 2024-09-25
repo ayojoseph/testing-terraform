137e3fbb85c983f9d5bc483d3bb1f4012d54d45b
ayojoseph <ayojoseph15@gmail.com>
2024-09-25 09:10:53 -0400

    added asg

--- main.tf
+++ main.tf
@@ -34,18 +34,37 @@ module "blog_vpc" {
 }
 
 #INSTANCES
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+# resource "aws_instance" "blog" {
+#   ami           = data.aws_ami.app_ami.id
+#   instance_type = var.instance_type
 
-  # vpc_security_group_ids = [aws_security_group.blog.id]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+#   # vpc_security_group_ids = [aws_security_group.blog.id]
+#   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+#   subnet_id = module.blog_vpc.public_subnets[0]
   
-  tags = {
-    Name = "HelloWorld"
-  }
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
+
+#AUTOSCALING
+
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+  
+
+  # Autoscaling group
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id = data.aws_ami.app_ami.id
 }
 
 # SECURITY GROUPS
